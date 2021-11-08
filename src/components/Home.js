3872454de06a62b1e601fbf1f007dc78f9c521fd
Jesse Maretz <jesse.maretz@gmail.com>
2021-11-08 17:26:01 -0500

    working in navmodal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './Footer';
 import NavModal from './NavModal'
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <h1>Home Body</h1>
             <NavModal />
+            <Footer />
         </div>
     )
 }
--- src/components/NavModal.js
+++ src/components/NavModal.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
 const NavModal = () => {
-    console.log('This is the nav')
-    const words = ['sky', 'blue', 'falcon', 'wood', 'cloud'];
-    const items = words.map((word, idx) => {
-    return <li key={idx}>{word}</li>;
-    });
-    console.log(items);
 
     return (
         <div>
-            <ul>{items}</ul>
+            p
         </div>
     )
 }
