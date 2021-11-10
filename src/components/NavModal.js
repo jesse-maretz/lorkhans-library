0e21314c0f61e92f6bb395b4aa11727b4a4c9e6f
Jesse Maretz <jesse.maretz@gmail.com>
2021-11-09 22:39:01 -0500

    adding navModal list

--- src/components/NavModal.js
+++ src/components/NavModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../style/navModal.css'
 const NavModal = () => {
 
     return (
@@ -7,6 +7,13 @@ const NavModal = () => {
             <button id="navModalButton">Hamburger menu</button>
             <div>
                 <p>NavModal</p>
+                <div id="listDiv">
+                    <ol>
+                        <li>A</li>
+                        <li>List</li>
+                        <li>Here</li>
+                    </ol>
+                </div>
             </div>
         </div>
     )
