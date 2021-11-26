6706fb1cbf6a49e79809b5722879f1f397508f3a
Jesse Maretz <jesse.maretz@gmail.com>
2021-11-26 18:14:26 -0500

    organized image folders

--- src/components/RaceLore.js
+++ src/components/RaceLore.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ViewRace from './ViewRace';
-import dunmerImg from '/Users/jessemaretz/Desktop/projects/scrolls-site/lorkhans-library/src/images/dunmerImg.png';
-import nordImg from '/Users/jessemaretz/Desktop/projects/scrolls-site/lorkhans-library/src/images/nordImg.png';
-import altmerImg from '/Users/jessemaretz/Desktop/projects/scrolls-site/lorkhans-library/src/images/altmerImg.png';
+import dunmerChar from '/Users/jessemaretz/Desktop/projects/scrolls-site/lorkhans-library/src/images/race-lore-imgs/dunmerChar.png';
+import nordChar from '/Users/jessemaretz/Desktop/projects/scrolls-site/lorkhans-library/src/images/race-lore-imgs/nordChar.png';
+import altmerChar from '/Users/jessemaretz/Desktop/projects/scrolls-site/lorkhans-library/src/images/race-lore-imgs/altmerChar.png';
 
 const RaceBios = () => {
     let raceArr = [
-        {race: 'Dunmer', img: dunmerImg, homeProv: 'Morrowind', religion: 'The Reclamations: Boethiah, Mephala, and Azura', background: '', index: 0},
-        {race: 'Nord', img: nordImg, homeProv: 'Skyrim', religion: 'The 9 Divines (Talos worship)', background: '', index: 1},
-        {race: 'Altmer', img: altmerImg, homeProv: 'Summerset Isles', religion: 'Altmeri Pantheon', background: '', index: 2}
+        {race: 'Dunmer', img: dunmerChar, homeProv: 'Morrowind', religion: 'The Reclamations: Boethiah, Mephala, and Azura', background: '', index: 0},
+        {race: 'Nord', img: nordChar, homeProv: 'Skyrim', religion: 'The 9 Divines (Talos worship)', background: '', index: 1},
+        {race: 'Altmer', img: altmerChar, homeProv: 'Summerset Isles', religion: 'Altmeri Pantheon', background: '', index: 2}
     ]
 
     return(
