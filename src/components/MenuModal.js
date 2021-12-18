ba240c0a71fd5919b7aa22e4335582a428c7dcb2
Jesse Maretz <jesse.maretz@gmail.com>
2021-12-17 22:36:05 -0500

    working on menu

--- src/components/MenuModal.js
+++ src/components/MenuModal.js
@@ -2,35 +2,14 @@ import React from 'react';
 import '../style/MenuModal.css'
 
 
-function MenuModal({props, visible}) {
+function MenuModal() {
 
     
 
     return (
-        <div
-        className="menu-modal"
-        style={{
-            backgroundColor: "rgba(0, 0, 0, .7)",
-            height: "100vh",
-            width: "100vw",
-            padding: 0,
-            margin: 0,
-            display: visible ? "flex" : "none",
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
-                <section
-                id="modal-menu"
-                style={{
-                    backgroundColor: "white",
-                    height: "300px",
-                    width: "300px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column"
-                  }}>
-                      <h3>{props}</h3>
+        <div className="menu-modal">
+                <section id="modal-menu">
+                      <h3>Poop</h3>
                 </section>
             </div>
     )
